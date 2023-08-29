e5d0ac1938a68975079b536a04851b47a9d5abac
Mabingtao <devforma@hotmail.com>
2023-08-29 14:14:25 +0800

    update default api baseurl

--- citybrain_platform/__init__.py
+++ citybrain_platform/__init__.py
@@ -3,7 +3,7 @@ from citybrain_platform.computing import Computing
 from citybrain_platform.computing.data_types import ColumnType, Column, JobStatus
 
 api_key = __os.getenv("CITYBRAIN_APIKEY")
-api_baseurl = __os.getenv("CITYBRAIN_API_BASEURL", "https://citybrain.org/platform/")
+api_baseurl = __os.getenv("CITYBRAIN_API_BASEURL", "https://www.citybrain.org/platform/")
 
 __all__ = [
     "Computing", 
